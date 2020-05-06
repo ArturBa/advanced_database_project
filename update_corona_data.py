7b2d16080aea1e48f0c8328cef2663274f80f0c2
Artur Bauer <bauerart1@gmail.com>
2020-05-06 12:46:14 +0200

    Fix update database

--- update_corona_data.py
+++ update_corona_data.py
@@ -1,10 +1,12 @@
-import urllib.request, json 
-from datetime import date, datetime
+import json
+import urllib.request
+from datetime import date, datetime, timedelta
+
 from sqlalchemy.orm import sessionmaker
+
 from engine import engine
 from model import CoronaVirus, Countries, Dates
 from utils import get_or_create
-
 
 # Start a session
 Session = sessionmaker(bind=engine)
@@ -12,10 +14,11 @@ session = Session()
 
 # Get last update date from db
 dates = session.query(Dates).all()
-dates_no_virus = list(filter(lambda date: date.corona_virus==[], dates))
-if dates_no_virus: 
-    first_no_virus_day = dates_no_virus[0].date
-else: 
+print(dates[0])
+dates_virus = list(filter(lambda date: date.corona_virus != [], dates))
+if dates_virus:
+    first_no_virus_day = dates_virus[-1].date + timedelta(days=1)
+else:
     first_no_virus_day = date.fromordinal(1)
 
 # get json data about coronavirus in counties
@@ -28,19 +31,17 @@ with urllib.request.urlopen("https://pomber.github.io/covid19/timeseries.json") 
             day_date = datetime.strptime(day['date'], '%Y-%m-%d').date()
 
             # Check the current date with last in db
-            if (first_no_virus_day < day_date):
+            if first_no_virus_day < day_date:
                 # If the json date is newer add to db
 
                 day_db = get_or_create(session, Dates, date=day_date)
 
-
                 record = CoronaVirus(
-                            country=contry_db, 
-                            date=day_db,
-                            confirmed=day['confirmed'],
-                            deaths=day['deaths'],
-                            recovered=day['recovered'])
+                    country=contry_db,
+                    date=day_db,
+                    confirmed=day['confirmed'],
+                    deaths=day['deaths'],
+                    recovered=day['recovered'])
                 session.add(record)
         session.commit()
         print(f'{country} added')
-
